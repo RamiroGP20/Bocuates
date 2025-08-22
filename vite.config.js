691dbaf7f6f09b2b202aa481081dd636fd359ada
RamiroGP20 <ramiroegr@yahoo.com>
2025-08-22 08:57:37 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// 👇 reemplaza "mi-empresa" por el nombre EXACTO de tu repositorio en GitHub
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-});
+  plugins: [react()],
+  base: '/Bocuates/'
+})
